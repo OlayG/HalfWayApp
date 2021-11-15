d3a885a7f066bbc96e4b41abb83fbba69b3ed4d0
Jimmy McBride <mcbride967@gmail.com>
2021-11-15 15:06:01 -0500

    added a constants file to store shared pref name keys

---
+++ app/src/main/java/com/olayg/halfwayapp/constants/Constants.kt
@@ -0,0 +1,7 @@
+package com.olayg.halfwayapp.constants
+
+object Constants {
+    const val gifList = "CHARACTER_GIF_LIST"
+    const val charName = "CHARACTER_NAME"
+    const val charGuide = "CHARACTER_GUIDE"
+}
