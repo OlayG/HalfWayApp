993a70eb2f8ec603a8ec3c4feb0cfb1388041009
Jimmy McBride <mcbride967@gmail.com>
2021-11-14 23:46:53 -0500

    refactored so Character model would work with new functionality

--- app/src/main/java/com/olayg/halfwayapp/model/custom/Character.kt
+++ app/src/main/java/com/olayg/halfwayapp/model/custom/Character.kt
@@ -3,24 +3,27 @@ package com.olayg.halfwayapp.model.custom
 import com.olayg.halfwayapp.model.response.CharacterResponse
 import com.olayg.halfwayapp.model.response.Gif
 import com.olayg.halfwayapp.model.response.Image
+import java.io.Serializable
 
 data class Character(
     val name: String,
+    val guide: String,
     val image: Image?,
     val rank: String,
     val weightRank: String,
     val speedRank: String,
-    val gifs: List<Gif>
-) {
+    val gifs: List<Gif>?
+) : Serializable {
 
     companion object {
-        fun convertToCharacter(characterResponse: CharacterResponse, image: Image?) = Character(
+        fun convertToCharacter(characterResponse: CharacterResponse, image: Image?, gifs: List<Gif>?) = Character(
             name = characterResponse.name,
+            guide = characterResponse.guide,
             image = image,
             rank = characterResponse.tierData,
             weightRank = characterResponse.weight,
             speedRank = characterResponse.fallSpeed,
-            gifs = characterResponse.gifs
+            gifs = gifs
         )
     }
 }
