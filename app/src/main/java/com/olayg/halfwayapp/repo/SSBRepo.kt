a09c431e87bbc398e76888f77ed2c2689c855548
Jimmy McBride <mcbride967@gmail.com>
2021-11-14 23:57:33 -0500

    added gfycat service to get urls

--- app/src/main/java/com/olayg/halfwayapp/repo/SSBRepo.kt
+++ app/src/main/java/com/olayg/halfwayapp/repo/SSBRepo.kt
@@ -2,16 +2,18 @@ package com.olayg.halfwayapp.repo
 
 import com.olayg.halfwayapp.model.custom.Character
 import com.olayg.halfwayapp.model.response.CharacterResponse
+import com.olayg.halfwayapp.model.response.Gif
 import com.olayg.halfwayapp.repo.remote.RetrofitInstance
 
 object SSBRepo {
 
     private val smashBrosUnofficialService by lazy { RetrofitInstance.smashBrosUnofficialService }
     private val smashLoungeService by lazy { RetrofitInstance.smashLoungeService }
+    private val gfyService by lazy { RetrofitInstance.gfyCatService }
 
     suspend fun getAllCharacters() = try {
         smashLoungeService.getAllCharacters().map { characterResponse ->
-            Character.convertToCharacter(characterResponse, getImage(characterResponse))
+            Character.convertToCharacter(characterResponse, getImage(characterResponse), getGfy(characterResponse.gifs))
         }
     } catch (ex: Exception) {
         emptyList()
@@ -23,6 +25,15 @@ object SSBRepo {
             name = "Mr. Game & Watch"
         }
         smashBrosUnofficialService.getAllCharacters(name).firstOrNull()?.image
+    } catch (ex: Exception) {
+        null
+    }
+
+    private suspend fun getGfy(gifs: List<Gif>) = try {
+        gifs.map { gif ->
+            gif.url = gfyService.getGfyCat(gif.url).gfyItem.max5mbGif
+            gif
+        }
     } catch (ex: Exception) {
         null
     }
