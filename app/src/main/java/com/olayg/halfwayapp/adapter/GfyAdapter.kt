65a21a08323d0c4a6f2b7f861ab5bab88b09c417
Jimmy McBride <mcbride967@gmail.com>
2021-11-14 23:51:20 -0500

    added adapter for new smash bro char gif list

---
+++ app/src/main/java/com/olayg/halfwayapp/adapter/GfyAdapter.kt
@@ -0,0 +1,38 @@
+package com.olayg.halfwayapp.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.olayg.halfwayapp.databinding.ItemGifBinding
+import com.olayg.halfwayapp.model.response.Gif
+import com.olayg.halfwayapp.util.layoutInflater
+import com.olayg.halfwayapp.util.loadUrl
+
+class GfyAdapter(
+    private val gifs: List<Gif>,
+) : RecyclerView.Adapter<GfyAdapter.GfyViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ) = GfyViewHolder.getInstance(parent)
+
+    override fun onBindViewHolder(holder: GfyViewHolder, position: Int) {
+        holder.loadGif(gifs[position])
+    }
+
+    override fun getItemCount() = gifs.size
+
+    class GfyViewHolder(
+        private val binding: ItemGifBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun loadGif(gif: Gif) = with(binding) {
+            ivGif.loadUrl(gif.url)
+        }
+
+        companion object {
+            fun getInstance(parent: ViewGroup) = ItemGifBinding.inflate(
+                parent.layoutInflater, parent, false
+            ).run { GfyViewHolder(this) }
+        }
+    }
+}
