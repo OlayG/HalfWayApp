5cbb75f5f63a9f60e96650763afe6d177049edf5
Jimmy McBride <mcbride967@gmail.com>
2021-11-16 10:44:29 -0500

    added handler for list of strings for a character gif url list

--- app/src/main/java/com/olayg/halfwayapp/repo/local/DataStorePref.kt
+++ app/src/main/java/com/olayg/halfwayapp/repo/local/DataStorePref.kt
@@ -3,6 +3,7 @@ package com.olayg.halfwayapp.repo.local
 import android.content.Context
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
 import com.olayg.halfwayapp.view.dataStore
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
@@ -10,10 +11,12 @@ import kotlinx.coroutines.flow.map
 class DataStorePref(val context: Context) {
     private val charName = stringPreferencesKey("character_name")
     private val charGuide = stringPreferencesKey("character_guide")
-    suspend fun setCharStringData(name: String, guide: String) {
+    private val charGifList = stringSetPreferencesKey("character_gifs")
+    suspend fun setCharStringData(name: String, guide: String, gifs: Set<String>) {
         context.dataStore.edit { preferences ->
             preferences[charName] = name
             preferences[charGuide] = guide
+            preferences[charGifList] = gifs
         }
     }
 
@@ -26,6 +29,12 @@ class DataStorePref(val context: Context) {
     suspend fun getCharGuide(): String? {
         return context.dataStore.data.map {
             it[charGuide]
+        }.first()
+    }
+
+    suspend fun getCharGifList(): Set<String>? {
+        return context.dataStore.data.map {
+            it[charGifList]
         }.first()
     }
 }
