ba7f6f3114d032129f9da7021a8102a8a17b6da5
Jimmy McBride <mcbride967@gmail.com>
2021-11-15 23:23:54 -0500

    added datastore to context

--- app/src/main/java/com/olayg/halfwayapp/view/MainActivity.kt
+++ app/src/main/java/com/olayg/halfwayapp/view/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.olayg.halfwayapp.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.olayg.halfwayapp.R
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
